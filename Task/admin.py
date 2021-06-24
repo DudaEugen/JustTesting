4303685efe6fc6f6e9aeb5046e86bebbe0f9fb30
PolyakAnton <54473424+PolyakAnton@users.noreply.github.com>
2021-06-24 20:19:04 +0300

    register TaskList and MultiplyChoiceTask to admin

--- Task/admin.py
+++ Task/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class AnswerForMultiplyCMultiplyChoiceTestAnswerInline(admin.TabularInline):
+    model = MultiplyChoiceTestAnswer
+    extra = 0
+    verbose_name = "Варіант відповіді"
+    verbose_name_plural = "Варіанти відповіді"
+
+
+class TaskListAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+    )
+    search_fields = (
+        "name",
+    )
+
+
+class MultiplyChoiceTestAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "__str__",
+        "task_list",
+    )
+    search_fields = (
+        "text",
+    )
+    inlines = (
+        AnswerForMultiplyCMultiplyChoiceTestAnswerInline,
+    )
+
+
+admin.site.register(TaskList, TaskListAdmin)
+admin.site.register(MultiplyChoiceTest, MultiplyChoiceTestAdmin)
