bd8d7b849df2424c49d1c35f3672b35a03c55b0a
PolyakAnton <54473424+PolyakAnton@users.noreply.github.com>
2021-06-28 16:28:29 +0300

    replace js functions for inline formsets

--- JustTesting/settings.py
+++ JustTesting/settings.py
@@ -131,6 +131,10 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'JustTesting/static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
