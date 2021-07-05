ed859ed17cad0777baf9ef2b1d8af55e5d5b7e86
PolyakAnton <54473424+PolyakAnton@users.noreply.github.com>
2021-07-05 22:18:47 +0300

    rewrite inline_formset.js to oop style

--- JustTesting/static/JustTesting/js/inline_formset.js
+++ JustTesting/static/JustTesting/js/inline_formset.js
@@ -1,42 +1,52 @@
-var inline_form_template = null
-var inline_form_conteiner = null
-var prefixes = null
-var inline_form_max_index = 0
+const inline_form_attr = "data-inline-prefix";
 
-function set_inline_form_template(container_class, prefix) {
-    prefixes = [prefix, "id_" + prefix];
-    let form = document.getElementsByClassName(container_class)[0];
-    inline_form_conteiner = form.parentNode;
-    inline_form_template = form.cloneNode(true);
-    inline_form_max_index = document.getElementsByClassName(inline_form_template.className).length - 1;
-}
+class InlineFormset {
+    constructor(template_element) {
+        let prefix = template_element.getAttribute(inline_form_attr);
+        this.prefixes = [prefix, "id_" + prefix];
+        this.container = template_element.parentNode;
+        this.form = template_element.cloneNode(true);
+        this.max_index = this.container.querySelectorAll('[' + inline_form_attr + ']').length - 1;
+    }
 
-function change_attributes(nodes, index) {
-    for (let n = 0; n < nodes.length; n++) {
-        let node = nodes[n];
-        if (node.childNodes)
-            change_attributes(node.childNodes, index);
-        if (node.attributes) {
-            for (let i = 0; i < node.attributes.length; i++) {
-                let attrib = node.attributes[i];
-                if (attrib.specified) {
-                    for (let p = 0; p < prefixes.length; p++) {
-                        let pref = prefixes[p];
-                        if (attrib.value.substring(0, pref.length) === pref) {
-                            attrib.value = pref + String(index) +
-                                attrib.value.substring(pref.length + 1, attrib.value.length);
-                            break;
+    change_attributes(nodes) {
+        for (let n = 0; n < nodes.length; n++) {
+            let node = nodes[n];
+            if (node.childNodes)
+                this.change_attributes(node.childNodes);
+            if (node.attributes) {
+                for (let i = 0; i < node.attributes.length; i++) {
+                    let attrib = node.attributes[i];
+                    if (attrib.specified) {
+                        for (let p = 0; p < this.prefixes.length; p++) {
+                            let pref = this.prefixes[p];
+                            if (attrib.value.substring(0, pref.length) === pref && attrib.name != inline_form_attr) {
+                                attrib.value = pref + String(this.max_index) +
+                                    attrib.value.substring(pref.length + 1, attrib.value.length);
+                                break;
+                            }
                         }
                     }
                 }
             }
         }
     }
+
+    add_inline_form() {
+        let new_form = this.form.cloneNode(true);
+        this.container.appendChild(new_form);
+        this.max_index += 1;
+        this.change_attributes(new_form.childNodes);
+        document.getElementById(this.prefixes[1] + "TOTAL_FORMS").setAttribute("value", this.max_index + 1);
+        console.log(this.max_index);
+    }
 }
 
-function add_inline_form() {
-    let new_form = inline_form_template.cloneNode(true);
-    inline_form_conteiner.appendChild(new_form);
-    change_attributes(new_form.childNodes, ++inline_form_max_index);
-    document.getElementById(prefixes[1] + "TOTAL_FORMS").setAttribute("value", inline_form_max_index + 1);
+var inline_formsets = new Map();
+
+function add_inline_formset() {
+    let forms = document.querySelectorAll('[' + inline_form_attr + ']');
+    for (let i = 0; i < forms.length; ++i) {
+        inline_formsets.set(forms[i].getAttribute(inline_form_attr), new InlineFormset(forms[i]));
+    }
 }
