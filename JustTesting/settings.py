da22cd5dd64bb1e902c61368bf10657b965efec6
PolyakAnton <54473424+PolyakAnton@users.noreply.github.com>
2021-07-10 13:36:53 +0300

    create and register app: Test

--- JustTesting/settings.py
+++ JustTesting/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Task.apps.TaskConfig',
+    'Test.apps.TestConfig',
 ]
 
 MIDDLEWARE = [
--- JustTesting/urls.py
+++ JustTesting/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('task/', include('Task.urls'))
+    path('task/', include('Task.urls')),
+    path('test/', include('Test.urls')),
 ]
 
 if settings.DEBUG:
