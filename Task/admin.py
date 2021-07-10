d01b6016b8e77316b3b6cc52169dba50806648b1
PolyakAnton <54473424+PolyakAnton@users.noreply.github.com>
2021-07-10 15:09:39 +0300

    register Test model in admin panel

--- Task/admin.py
+++ Task/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 from .forms import MultiplyChoiceTestAnswerInlineFormset
+from Test.models import Test
 
 
 class MultiplyChoiceTestAnswerInline(admin.TabularInline):
@@ -11,12 +12,24 @@ class MultiplyChoiceTestAnswerInline(admin.TabularInline):
     verbose_name_plural = "Варіанти відповіді"
 
 
+class TestInTaskListInline(admin.TabularInline):
+    model = Test.task_lists.through
+    extra = 0
+    verbose_name = "Список питань"
+    verbose_name_plural = "Списки питань"
+    readonly_fields = ("test", "task_count")
+    can_delete = False
+
+
 class TaskListAdmin(admin.ModelAdmin):
     list_display = (
         "__str__",
     )
     search_fields = (
         "name",
+    )
+    inlines = (
+        TestInTaskListInline,
     )
 
 
--- Test/admin.py
+++ Test/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class TaskListInTestInline(admin.TabularInline):
+    model = Test.task_lists.through
+    extra = 0
+    verbose_name = "Список питань"
+    verbose_name_plural = "Списки питань"
+
+
+class TestAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "is_allowed",
+        "is_allow_for_unautorized_users",
+    )
+    search_fields = (
+        "name",
+    )
+    list_filter = (
+        "is_allowed",
+        "is_allow_for_unautorized_users",
+    )
+    inlines = (
+        TaskListInTestInline,
+    )
+
+
+admin.site.register(Test, TestAdmin)
