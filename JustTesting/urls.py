e4f82847d557ba0f89355ff1f1da32d8abe04f93
Duda Eugen <duda.evgen@gmail.com>
2021-08-14 21:39:30 +0300

    add login and logged out pages

--- JustTesting/urls.py
+++ JustTesting/urls.py
@@ -25,6 +25,11 @@ urlpatterns = [
     path('testing/', include('Testing.urls')),
 ]
 
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
