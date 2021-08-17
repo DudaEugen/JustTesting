1a59a8f7782bdf77ad24b16ca6dbc3a4898beb68
Duda Eugen <duda.evgen@gmail.com>
2021-08-17 07:07:57 +0300

    update MultipleChoiceTestSolutionForm.html - check answers before submit + replace all form from testing.html to task form template

--- Testing/static/Testing/js/multiple_choice_test.js
+++ Testing/static/Testing/js/multiple_choice_test.js
@@ -1,10 +1,29 @@
 function answer_click(answer_container) {
-    checkbox = answer_container.getElementsByTagName('input')[0];
+    let checkbox = answer_container.getElementsByTagName('input')[0];
     if (!answer_container.classList.contains('selected')) {
         answer_container.classList.add('selected');
         checkbox.checked = true;
     } else {
         answer_container.classList.remove('selected');
         checkbox.checked = false;
+    }
+}
+
+
+function solution_check(e, form) {
+    let inputs = form.getElementsByTagName("input");
+    for (let i = 0; i < inputs.length; ++i) {
+        if (inputs[i].getAttribute("type") === "checkbox" && inputs[i].checked) {
+            return;
+        }
+    }
+    e.preventDefault();
+    let answers_container = form.children[2];
+    if (!answers_container.classList.contains("error_answers")) {
+        answers_container.classList.add("error_answers");
+        let paragraph = document.createElement("p");
+        paragraph.innerText = "Оберіть варіант відповіді";
+        paragraph.className = "error";
+        answers_container.insertBefore(paragraph, answers_container.firstChild);
     }
 }
