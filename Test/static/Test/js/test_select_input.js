cec1cb695a0be3de6bdf5bce01121d931bcb0257
Duda Eugen <duda.evgen@gmail.com>
2021-08-24 12:21:12 +0300

    update test_list page - add input list for search tests + create template for non-form select_input element

---
+++ Test/static/Test/js/test_select_input.js
@@ -0,0 +1,22 @@
+const linkPlaceholder = "12345678901234567890";
+
+function linkToTest(inputList) {
+    let link = document.getElementById("test_link");
+
+    let value = "";
+    let options = document.getElementById(inputList.getAttribute("list")).getElementsByTagName("option");
+    for (let i = 0; i < options.length; ++i) {
+        if (inputList.value == options[i].value) {
+            value = options[i].getAttribute("data-value");
+            break;
+        }
+    }
+
+    if (value) {
+        link.setAttribute("href", link.getAttribute("data-link").replace(
+            linkPlaceholder, value
+        ));
+    } else {
+        link.removeAttribute("href");
+    }
+}
